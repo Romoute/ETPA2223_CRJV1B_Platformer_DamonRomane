1ed6d3f15bbc77d289df298044ee67b53c1feda9
Romoute <r.damon@rns.ecolescreatives.com>
2023-05-27 22:01:23 +0200

    ouioui

---
+++ tileset/tileset_4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset_4" tilewidth="32" tileheight="32" tilecount="392" columns="28">
+ <image source="tileset_4.png" width="896" height="448"/>
+</tileset>
