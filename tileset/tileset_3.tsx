f8ee52e73683e89f7ab2e6fe06f213e17bd10154
Romoute <r.damon@rns.ecolescreatives.com>
2023-05-29 13:57:15 +0200

    oui?

--- tileset/tileset_3.tsx
+++ tileset/tileset_3.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="tileset_3" tilewidth="32" tileheight="32" tilecount="392" columns="28">
- <image source="../../../../Desktop/ETPA/2022-2023/MARIO LIKE/tileset_3.png" width="896" height="448"/>
+ <image source="tileset_3.png" width="896" height="448"/>
 </tileset>
